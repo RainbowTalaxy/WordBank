22feb4c15adf1a5143eee0140c4961ae1a3c4adb
Talaxy <siwei.chen@shanbay.com>
2023-04-12 14:35:14 +0800

    Initial Commit

---
+++ WordBank/WordBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBankApp.swift
+//  WordBank
+//
+//  Created by Talaxy on 2023/4/12.
+//
+
+import SwiftUI
+
+@main
+struct WordBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
